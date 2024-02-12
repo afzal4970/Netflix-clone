a1a5de1d301db254d86948fa14d52df949b8c42b
Afzal Ahmad <afzalahmad@afzal.ahmad>
2024-02-12 16:10:12 +0530

    netflix clone react

--- src/App.js
+++ src/App.js
@@ -1,24 +1,26 @@
-
-import './App.css';
-import Row from './Row';
-import requests from './requests'
-import Banner from './Banner';
-import Nav from './Nav';
+import "./App.css";
+import Row from "./Row";
+import requests from "./requests";
+import Banner from "./Banner";
+import Nav from "./Nav";
 
 function App() {
   return (
     <div className="app">
-      <Nav/>
-     <Banner/>
-     <Row title="NETFLIX ORIGINALS" fetchUrl={requests.fetchNetflixOriginals} isLargeRow={true}/>
-     <Row title="Trending Now" fetchUrl={requests.fetchTrending}/>
-     <Row title="Top Rated" fetchUrl={requests.fetchTopReated}/>
-     <Row title="Action Movies" fetchUrl={requests.fetchActionMovies}/>
-     <Row title="Comedy Movies" fetchUrl={requests.fetchComedyMovies}/>
-     {/* <Row title="Horror Movies" fetchUrl={requests.fetchHorrorMovies}/> */}
-     <Row title="Romance Movies" fetchUrl={requests.fetchRmomanceMovies}/>
-     <Row title="Documentaries" fetchUrl={requests.fetchDocumentaries}/>
-     
+      <Nav />
+      <Banner />
+      <Row
+        title="NETFLIX ORIGINALS"
+        fetchUrl={requests.fetchNetflixOriginals}
+        isLargeRow={true}
+      />
+      <Row title="Trending Now" fetchUrl={requests.fetchTrending} />
+      <Row title="Top Rated" fetchUrl={requests.fetchTopReated} />
+      <Row title="Action Movies" fetchUrl={requests.fetchActionMovies} />
+      <Row title="Comedy Movies" fetchUrl={requests.fetchComedyMovies} />
+      {/* <Row title="Horror Movies" fetchUrl={requests.fetchHorrorMovies}/> */}
+      <Row title="Romance Movies" fetchUrl={requests.fetchRmomanceMovies} />
+      <Row title="Documentaries" fetchUrl={requests.fetchDocumentaries} />
     </div>
   );
 }
